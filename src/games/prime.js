8cf1f6220c4f1ecbd7feaff977d5081ff92d9ece
lmilosz <neresltvn1@gmail.com>
2023-05-24 00:01:51 +0300

    create brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import runGameLogic from '../gameLogic.js';
+
+import generateRandomNumber from '../utils.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+const startRound = () => {
+    const randomNumber = generateRandomNumber(2,100);
+    const question = `Question: ${randomNumber}`;
+    const correctAnswer = (isPrime(randomNumber) ? 'yes' : 'no');
+    return [question, correctAnswer];
+  };
+  const runPrimeGame = () => runGameLogic(rule, startRound);
+  
+  export default runPrimeGame;
