49dd2a3ac67c8b15db200d8263d86418e6119d2b
lmilosz <neresltvn1@gmail.com>
2023-05-23 21:26:47 +0300

    make lint on step 7

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,22 +3,22 @@ import generateRandomNumber from '../utils.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const getGcd  = (num1, num2) => {
-    if (num2 > num1) return getGcd(num2, num1);
-    if (!num2) return num1;
-    return getGcd (num2, num1 % num2);
-  };
+const getGcd = (num1, num2) => {
+  if (num2 > num1) return getGcd(num2, num1);
+  if (!num2) return num1;
+  return getGcd(num2, num1 % num2);
+};
 
-  const startRound = () => {
-    const number1 = generateRandomNumber(1,100);
-    const number2 = generateRandomNumber(1,100);
-    const question = `${number1} ${number2}`;
-    const trueAnswer = String(getGcd(number1, number2));
-    return [question, trueAnswer];
-  };
+const startRound = () => {
+  const number1 = generateRandomNumber(1, 100);
+  const number2 = generateRandomNumber(1, 100);
+  const question = `${number1} ${number2}`;
+  const trueAnswer = String(getGcd(number1, number2));
+  return [question, trueAnswer];
+};
 
-  const runGcd = () => {
-    runGameLogic(rules, startRound);
-  };
+const runGcd = () => {
+  runGameLogic(rules, startRound);
+};
 
 export default runGcd;
